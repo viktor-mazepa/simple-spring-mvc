9059e03bbd6854f2f4b6d556a5eb638f9fa66852
vima1013 <viktor.mazepa@NetCracker.com>
2023-03-06 16:54:13 +0200

    Remove xml configuration. Add java-code base configuration

---
+++ src/main/java/com/mazasoft/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,18 @@
+package com.mazasoft.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
